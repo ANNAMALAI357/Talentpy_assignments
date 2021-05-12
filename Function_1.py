28ad29acdaa776d8f8cfbfa1545a08714f362745
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2021-05-12 15:54:30 +0530

    Update Function_1.py

--- Function_1.py
+++ Function_1.py
@@ -1,3 +1,8 @@
+"""
+John would like to create a function to check whether given number is
+positive or negative or neutral. Help him to write an independent function
+and pass different inputs to the function and check its behaviour
+"""
 def numCheck(n):
     if(n<0):
         return "Negative"
