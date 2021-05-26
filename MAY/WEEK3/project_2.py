3084e55a37e2d88bfab298914955d786ee9e1a0f
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2021-05-26 08:59:02 +0530

    factorial of even numbers

--- MAY/WEEK3/project_2.py
+++ MAY/WEEK3/project_2.py
@@ -1,6 +1,7 @@
 """Write a list comprehension to find factorial of each numbers in a given list L if and only
 if the numbers are even. For Example: If L = [1,2,3,4] then output should be [2, 24]."""
 
+
 def factorial(x):
     fact = 1
     for y in range(1,x + 1):
