8f8a14ca14ec9d02a743c671f343e4568779ae51
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2021-05-26 08:58:26 +0530

    power

--- MAY/WEEK3/project_1.py
+++ MAY/WEEK3/project_1.py
@@ -3,6 +3,7 @@ Create a lambda function which takes two inputs X and Y and performs X power Y
 operation and returns the output. For Example: If X is 2 and Y is 3, then 2 power 3 = 2
 * 2 * 2 = 8."""
 
+
 lam_func = lambda X,Y : X**Y
 X = int(input("Enter the the base value"))
 Y = int(input("Enter the the exponent value))
