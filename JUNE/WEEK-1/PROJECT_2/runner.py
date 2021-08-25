48cf84017bbc1df8fafee06f435efb38b8ab7aa7
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2021-08-25 17:47:28 +0530

    runner.py

--- JUNE/WEEK-1/PROJECT_2/runner.py
+++ JUNE/WEEK-1/PROJECT_2/runner.py
@@ -1,1 +1,17 @@
-
+from main import OperatingSystem as OS
+import os
+import shutil
+mem = OS()
+print("Please the os operation you want to do:")
+print("""1.directory of the given path 
+2.Show the current directory
+3.copy files from one folder to another""")
+i = int(input())
+if(i==1):
+    print(mem.show_directories())
+elif(i==2):
+    print(mem.get_current_directory())
+elif(i==3):
+    print(mem.copyFile())
+else:
+    print("Invalid option")
