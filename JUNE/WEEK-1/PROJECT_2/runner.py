3c068a3b04f2d08b3dd272462c84759aba6b39af
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2021-08-26 17:17:21 +0530

    runner.py

--- JUNE/WEEK-1/PROJECT_2/runner.py
+++ JUNE/WEEK-1/PROJECT_2/runner.py
@@ -1,6 +1,4 @@
 from main import OperatingSystem as OS
-import os
-import shutil
 mem = OS()
 print("Please the os operation you want to do:")
 print("""1.directory of the given path 
@@ -8,10 +6,13 @@ print("""1.directory of the given path
 3.copy files from one folder to another""")
 i = int(input())
 if(i==1):
-    print(mem.show_directories())
+    Path = input("Enter the path to be searched: ")
+    print(mem.show_directories(Path))
 elif(i==2):
     print(mem.get_current_directory())
 elif(i==3):
-    print(mem.copyFile())
+    source = input("enter the source of the file to be copied: ")
+    destination = input("enter the destination of the file: ")
+    print(mem.copyFile(source,destination))
 else:
     print("Invalid option")
